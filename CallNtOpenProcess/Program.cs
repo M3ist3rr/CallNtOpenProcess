e1a8064d837c2e21c32014ccd614fa4e779dd5bd
M3ist3rr <meister3441@protonmail.com>
2023-06-26 20:07:29 -0700

    CallNtOpenProcess

---
+++ CallNtOpenProcess/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using static CallNtOpenProcess.Native;
+
+namespace CallNtOpenProcess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = Process.GetProcessesByName("notepad")[0];
+            CLIENT_ID ci = new CLIENT_ID
+            {
+                UniqueProcess = (IntPtr)process.Id
+            };
+
+            OBJECT_ATTRIBUTES oa = new OBJECT_ATTRIBUTES();
+
+            IntPtr hProcess = IntPtr.Zero;
+
+            NtOpenProcess(ref hProcess, (uint)ProcessAccessFlags.All, ref oa, ref ci);
+            Console.WriteLine("done");
+        }
+    }
+}
---
+++ CallNtOpenProcess/Syscall.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using static CallNtOpenProcess.Native;
+using System.ComponentModel;
+
+
+namespace CallNtOpenProcess
+{
+    class Syscall
+    {
+        static byte[] bNtOpenProcess =
+        {
+            0x4C, 0x8B, 0xD1,               // mov r10, rcx
+            0xB8, 0x26, 0x00, 0x00, 0x00,   // mov eax, 0x26 (NtOpenProcess Syscall)
+            0x0F, 0x05,                     // syscall
+            0xC3                            // ret
+        };
+
+        public static NTSTATUS NtOpenProcess(ref IntPtr ProcessHandle, UInt32 AccessMask, ref OBJECT_ATTRIBUTES ObjectAttributes, ref CLIENT_ID ClientId)
+        {
+            byte[] syscall = bNtOpenProcess;
+
+            unsafe
+            {
+                fixed (byte* p = syscall)
+                {
+                    IntPtr memoryAddress = (IntPtr)p;
+
+                    if(!VirtualProtect(memoryAddress, (UIntPtr)syscall.Length, 0x00000040, out uint lpflOldProtect))
+                    {
+                        throw new Win32Exception();
+                    }
+
+                    Delegates.NtOpenProcess assembledFunction = (Delegates.NtOpenProcess)Marshal.GetDelegateForFunctionPointer(memoryAddress, typeof(Delegates.NtOpenProcess));
+
+                    return assembledFunction(ref ProcessHandle, AccessMask, ref ObjectAttributes, ref ClientId);
+                }
+            }
+        }
+
+        public struct Delegates
+        {
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate NTSTATUS NtOpenProcess(ref IntPtr ProcessHandle, UInt32 AccessMask, ref OBJECT_ATTRIBUTES ObjectAttributes, ref CLIENT_ID ClientId);
+        }
+
+    }
+}
